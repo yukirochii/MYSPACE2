240f98cfc901a776f8ec874cdde98ff00ceb0a06
yukirochii <femustv@gmail.com>
2025-01-24 18:53:40 +0800

    C:\HTML CSS\MYSPACE

---
+++ MYSPACE/song.js
@@ -0,0 +1,81 @@
+export const songList = [
+    {
+        img: 'music/cover/about.jpg',
+        name: 'About You',
+        music: 'music/songs/About You.mp3',
+        artist:"The 1975"
+    },
+    {
+        img: 'music/cover/its.jpg',
+        name: 'Its Not Living',
+        music: 'music/songs/Its Not Living (If Its Not With You).mp3',
+        artist:"The 1975"
+    },
+    {
+        img: 'music/cover/robbers.jpg',
+        name: 'Robbers',
+        music: 'music/songs/Robbers.mp3',
+        artist:"The 1975"
+    }
+    ,
+    {
+        img: 'music/cover/multo.jpg',
+        name: 'Multo',
+        music: 'music/songs/multo.mp3',
+        artist:"Cup of joe"
+    }
+    ,
+    {
+        img: 'music/cover/untiunti.jpg',
+        name: 'unti-unti',
+        music: 'music/songs/untiunti.mp3',
+        artist:"Up Dharma Down"
+    }
+    ,
+    {
+        img: 'music/cover/wagnalang.jpg',
+        name: 'Wag Na Lang',
+        music: 'music/songs/wagnalang.mp3',
+        artist:"Cup of joe"
+    }
+    ,
+    {
+        img: 'music/cover/whiteferrari.jpg',
+        name: 'White Ferrari',
+        music: 'music/songs/whiteferrari.mp3',
+        artist: "Frank Ocean"
+    },
+    {
+        img: 'music/cover/backburner.jpg',
+        name: 'Backburner',
+        music: 'music/songs/backburner.mp3',
+        artist: "NIKI"
+    },
+   
+    {
+        img: 'music/cover/autumn.jpg',
+        name: 'Autumn',
+        music: 'music/songs/autumn.mp3',
+        artist: "NIKI"
+    },
+    {
+        img: 'music/cover/mananatili.jpg',
+        name: 'Mananatili',
+        music: 'music/songs/mananatili.mp3',
+        artist: "Cup of Joe"
+    },
+    {
+        img: 'music/cover/bluesky.jpg',
+        name: 'Blue Sky',
+        music: 'music/songs/bluesky.mp3',
+        artist: "Hale"
+    },
+    {
+     img: 'music/cover/fortnight.png',
+     name: 'Fortnight',
+     music: 'music/songs/fortnight.mp3',
+     artist: "Taylor Swift"
+    
+ }
+    
+ ]
